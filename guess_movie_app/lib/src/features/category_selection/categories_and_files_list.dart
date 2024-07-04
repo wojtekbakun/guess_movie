8988b5ca0d9d52289e72dc7c90cfe8b17317a229
wojtekbakun <wojciechzsee@gmail.com>
2024-07-04 23:29:58 +0200

    added new categories

--- guess_movie_app/lib/src/features/category_selection/categories_and_files_list.dart
+++ guess_movie_app/lib/src/features/category_selection/categories_and_files_list.dart
@@ -4,37 +4,31 @@ List<List<dynamic>> categoriesAndFilesList = [
   [
     'Actors',
     'assets/actors.json',
-    '20',
     Symbols.computer,
   ],
   [
     'Animals',
     'assets/animals.json',
-    '20',
     Symbols.bug_report,
   ],
   [
     'Birds',
     'assets/birds.json',
-    '20',
     Symbols.raven,
   ],
   [
     'Board Games',
     'assets/board_games.json',
-    '20',
     Symbols.car_repair,
   ],
   [
     'Bridges',
     'assets/bridges.json',
-    '20',
     Symbols.party_mode,
   ],
   [
     'Country Flags',
     'assets/country_flags.json',
-    '20',
     Symbols.play_circle,
   ],
 ];
